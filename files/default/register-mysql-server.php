95cb056985465ff5e650a3ffaa63e397cff693a2
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2015-11-21 17:54:10 +0800

    init provisioning user and register server

---
+++ files/default/register-mysql-server.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * usage
+ * php register-mysql-server.php --manage-api-url="https://syrup.keboola.com/provisioning/manage" \
+ *      --manage-token="TOKEN" \
+ *      --user=provisioning \
+ *      --password=provisioning \
+ *      --database=provisioning \
+ *      --host=db-server.keboola.com \
+ *      --type=transformations \
+ *      --mode=active
+ */
+
+$shortopts = "a:m:u:p:d:h:t:m:";
+$longopts = ["manage-api-url:", "manage-token:", "user:", "password:", "database:", "host:", "type:", "mode:"];
+
+$options = getopt($shortopts, $longopts);
+
+$command = 'curl -s -X "POST" "' . $options["manage-api-url"] . '/server/mysql" ';
+$command .= '-H "X-KBC-ManageApiToken: ' . $options["manage-token"] . '" ';
+$command .= '-H "Content-Type: application/x-www-form-urlencoded" ';
+$command .= '--data-urlencode "password=' . $options["password"] . '" ';
+$command .= '--data-urlencode "user=' . $options["user"] . '" ';
+$command .= '--data-urlencode "host=' . $options["host"] . '" ';
+$command .= '--data-urlencode "type=' . strtolower($options["type"]) . '" ';
+$command .= '--data-urlencode "mode=' . strtolower($options["mode"]) . '" ';
+$command .= '--data-urlencode "database=' . $options["database"] . '"';
+
+$responseString = exec($command);
+if (!$responseString) {
+    exit(1);
+}
+$response = json_decode($responseString, true);
+if (!$response) {
+    print $respons;
+    exit(1);
+}
+if (!isset($response["id"])) {
+    print $responseString;
+    exit(1);
+}
+if (!isset($response["isAlive"]) || !$response["isAlive"]) {
+    print $responseString;
+    exit(1);
+}
+
+$command = 'curl -s -X "POST" "' . $options["manage-api-url"] . '/server/mysql/' . $response["id"] . '/activate" ';
+$command .= '-H "X-KBC-ManageApiToken: ' . $options["manage-token"] . '" ';
+$command .= '-H "Content-Type: application/x-www-form-urlencoded" ';
+
+$responseString = exec($command);
+$responseString = exec($command);
+if (!$responseString) {
+    exit(1);
+}
+$response = json_decode($responseString, true);
+if (!$response) {
+    print $respons;
+    exit(1);
+}
+if (!isset($response["id"])) {
+    print $responseString;
+    exit(1);
+}
+if (!isset($response["isAlive"]) || !$response["isAlive"]) {
+    print $responseString;
+    exit(1);
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,12 +15,19 @@ cookbook_file "/etc/yum.repos.d/MariaDB.repo" do
     group "root"
 end
 
+cookbook_file "/tmp/register-mysql-server.php" do
+    source "register-mysql-server.php"
+    mode "0644"
+    owner "root"
+    group "root"
+end
+
 execute "clean yum cache" do
   command "yum clean all"
 end
 
 execute "install mariadb" do
-  command "yum -y install MariaDB-server MariaDB-client"
+  command "yum -y install php MariaDB-server MariaDB-client"
 end
 
 cookbook_file "/etc/my.cnf" do
@@ -40,4 +47,32 @@ end
 
 execute "set default password for root user" do
     command "/usr/bin/mysqladmin -u root password '#{node['keboola-transformation-db']['mysql']['default-password']}'"
+end
+
+execute "allow ssh tunnel root access" do
+    command "mysql -u root -p#{node['keboola-transformation-db']['mysql']['default-password']} -e \"SET PASSWORD FOR 'root'@'127.0.0.1' = PASSWORD('#{node['keboola-transformation-db']['mysql']['default-password']}');\"
+end
+
+execute "mysql_tzinfo" do
+    command "mysql_tzinfo_to_sql  /usr/share/zoneinfo/ | mysql -u root -p#{node['keboola-transformation-db']['mysql']['default-password']} mysql"
+end
+
+execute "install mkpasswd" do
+  command "yum -y install expect"
+end
+
+execute "store passwd to env" do
+    command "export PROVISIONING_PASSWORD=`mkpasswd -l 16`"
+end
+
+execute "create provisioning user" do
+    command "mysql -u root -p#{node['keboola-transformation-db']['mysql']['default-password']} -e \"GRANT ALL PRIVILEGES ON *.* TO 'provisioning'@'%' IDENTIFIED BY '$PROVISIONING_PASSWORD' WITH GRANT OPTION;\""
+end
+
+execute "create provisioning database" do
+    command "mysql -u root -p#{node['keboola-transformation-db']['mysql']['default-password']} -e \"CREATE DATABASE provisioning DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;\""
+end
+
+execute "register provisioning server"
+    command "php /tmp/register-mysql-server.php --api-url=\"#{node['keboola-transformation-db']['provisioning-manage-api']['url']\" --manage-token=\"#{node['keboola-transformation-db']['provisioning-manage-api']['token']\" --user=provisioning --password=$PROVISIONING_PASSWORD --database=provisioning --host=\"`hostname`.keboola.com\" --type=transformations --mode=active"
 end
