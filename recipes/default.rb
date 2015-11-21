b57f3dd3f543c3c631b2d17c370358fe77e95b7c
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2015-11-21 15:39:58 +0800

    feat: MariaDB repo

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,11 @@
 include_recipe "aws"
 include_recipe "keboola-common"
 include_recipe 'sysctl::apply'
+
+
+cookbook_file "/etc/yum.repos.d/MariaDB.repo" do
+    source "MariaDB.repo"
+    mode "0644"
+    owner "root"
+    group "root"
+end
