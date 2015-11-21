d7ed078f4586752b3be33d5e0211881d7990a690
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2015-11-21 16:28:30 +0800

    mariadb installation and start

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 default['rsyslog']['max_message_size'] = '32k'
+default['keboola-transformation-db']['mysql']['default-password'] = ''
+default['keboola-transformation-db']['provisioning-manage-api']['token'] = ''
+default['keboola-transformation-db']['provisioning-manage-api']['url'] = ''
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: keboola-syrup
+# Cookbook Name:: keboola-tapi-mysql
 # Recipe:: default
 #
 
@@ -13,4 +13,31 @@ cookbook_file "/etc/yum.repos.d/MariaDB.repo" do
     mode "0644"
     owner "root"
     group "root"
+end
+
+execute "clean yum cache" do
+  command "yum clean all"
+end
+
+execute "install mariadb" do
+  command "yum -y install MariaDB-server MariaDB-client"
+end
+
+cookbook_file "/etc/my.cnf" do
+    source "my.cnf"
+    mode "0644"
+    owner "root"
+    group "root"
+end
+
+execute "create log dirs" do
+    command "mkdir /var/log/mariadb"
+end
+
+execute "run mariadb" do
+    command "/etc/init.d/mysql start"
+end
+
+execute "set default password for root user" do
+    command "/usr/bin/mysqladmin -u root password '#{node['keboola-transformation-db']['mysql']['default-password']}'"
 end
